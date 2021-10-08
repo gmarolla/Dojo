6542dbf15f40986ca81441c30a0006814cd8b31a
HeniWaref <wheni@talentia-software.com>
2021-10-08 17:05:39 +0200

    Finish first test

--- Dojo NunitTest/PersonDAOTest.cs
+++ Dojo NunitTest/PersonDAOTest.cs
@@ -12,26 +12,29 @@ namespace Dojo_NunitTest
     public class PersonDAOTest
     {
         PersonDAO personDAO;
+        Mock<IDbConnection> mockDbConnection;
         [SetUp]
         public void Setup()
         {
             Mock<IDbDataParameter> mockIDbParameter = new Mock<IDbDataParameter>();
             IDbDataParameter DbParameter = mockIDbParameter.Object;
-
-
             Mock<IDbCommand> mockDbCommand = new Mock<IDbCommand>();
-            IDbCommand DbCommand = mockDbCommand.Object;.
+            IDbCommand DbCommand = mockDbCommand.Object;
+            Mock<IDataParameterCollection> mockDataParameterCollection = new Mock<IDataParameterCollection>();
+            mockDbCommand.Setup(x => x.Parameters).Returns(mockDataParameterCollection.Object);
             mockDbCommand.Setup(x => x.CreateParameter()).Returns(DbParameter);
+            mockDbCommand.Setup(x => x.ExecuteScalar()).Returns(1);
 
-            Mock<IDbConnection> mockDbConnection = new Mock<IDbConnection>();
+            mockDbConnection = new Mock<IDbConnection>();
             IDbConnection DbConnection = mockDbConnection.Object;
             mockDbConnection.Setup(x => x.CreateCommand()).Returns(DbCommand);
-            
+
 
             Mock<IConnectionManager> mockConnectionManager = new Mock<IConnectionManager>();
             IConnectionManager ConnectionManager = mockConnectionManager.Object;
 
             mockConnectionManager.Setup(x => x.CreateConnection()).Returns(DbConnection);
+
 
             personDAO = new PersonDAO(ConnectionManager);
         }
@@ -43,19 +46,15 @@ namespace Dojo_NunitTest
             string FirstName = "Giuseppe";
             string LastName = "Marolla";
             DateTime DateOfBirth = new DateTime(1989, 07, 14);
-
-
-
             PersonEntity personExpected = new PersonEntity(1, FirstName, LastName, DateOfBirth);
-
             //when
             PersonEntity personActual = personDAO.CreatePerson(FirstName, LastName, DateOfBirth);
-
             //then
             Assert.AreEqual(personExpected.ID, personActual.ID);
             Assert.AreEqual(personExpected.Name, personActual.Name);
             Assert.AreEqual(personExpected.Surname, personActual.Surname);
             Assert.AreEqual(personExpected.DateOfBirth, personActual.DateOfBirth);
+            mockDbConnection.Verify(x => x.Open(), Times.Exactly(1));
 
         }
     }
---
+++ Dojo/Iterfaces/IPersonDAO.cs
@@ -0,0 +1,14 @@
+﻿using Dojo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dojo.Iterfaces
+{
+   public  interface IPersonDAO
+    {
+        public PersonEntity CreatePerson(IDbConnection connection, string FirstName, string SecondName, DateTime DateOfBirth);
+    }
+}
